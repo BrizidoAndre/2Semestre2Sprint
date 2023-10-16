f4aa06572dff83543a5d41a2b95a885588978605
Andre <abrizidobasilio@gmail.com>
2023-10-16 11:01:37 -0300

    finalizando a calculadora de imc

--- Calculadora IMC/script.js
+++ Calculadora IMC/script.js
@@ -1,14 +1,19 @@
-function calcular(){
-    event.preventDefault();
+const arrPessoas = [];//array vazio
+
+function calcular(e){
+
+    e.preventDefault();
     
     // Pegar os dados do formulário
     // Validar se eles estõa prenchidos
-    let nome = document.getElementById("nome").value;
+    const nome = document.getElementById("nome").value.trim();
 
-    let altura = parseFloat(document.getElementById("altura").value);
+    const altura = parseFloat(document.getElementById("altura").value);
 
-    let peso = parseFloat(document.getElementById("peso").value);
+    const peso = parseFloat(document.getElementById("peso").value);
 
+    // Verifica se os dados recebidos estão corretos
+    // Senão retorna um alerta
     if (isNaN(altura) || isNaN(peso) || nome.length == 0) {
         alert("É necessário preencher os campos corretamente");
         // Colocamos um return vazio, para o método não continuar seguindo suas etapas aceitando um NaN
@@ -18,20 +23,31 @@ function calcular(){
     // Calcular o Imc
     const imc = calcularImc(altura, peso);
 
-    console.log(nome);
-    console.log(altura);
-    console.log(peso);
-    console.log(imc);
+    // gera o texto da situação
+    const situacao = retornaSituação(imc);
+    
+    let hoje = new Date().toLocaleString;
+    let dia = hoje.getDay() + 15;
+    let mes = hoje.getMonth() + 1;
+    let ano = hoje.getFullYear();
 
-    let situacao = retornaSituação(imc);
+    //Gerando o objeto Pessoa com seus respectivos dados
+    const pessoa = {nome, altura, peso, imc, situacao, dataCadastro: dia + "/" + mes + "/" + ano};//objeto vazio
 
-    console.log(situacao);
+
+    // Adiciona a pessoa na lista
+    arrPessoas.push(pessoa);
+
+    // listar as pessoas
+    listarPessoas();
 }
 
+// return peso / altura ** 2
 function calcularImc(altura, peso){
     return peso / Math.pow(altura, 2);
 }
 
+// Retorna a situação com base no imc
 function retornaSituação(imc) {
     if (imc < 18.5) {
         return 'Magreza Severa'
@@ -46,4 +62,32 @@ function retornaSituação(imc) {
     } else {
         return 'Obesidade III (Cuidado!)'
     }
+}
+
+// Retorna uma lista de pessoas cadastradas
+function listarPessoas() {
+
+    // Criando o array que aparecerá na listagem
+    let template = [];
+
+
+    arrPessoas.forEach(p => {
+        // Aqui para cada item do array de cadastros estou criando uma variável de texto que mostra suas respectivas informações
+        let cadastro =`
+        <tr>
+            <td>${p.nome}</td>
+            <td>${p.altura}</td>
+            <td>${p.peso}</td>
+            <td>${p.imc.toFixed(2)}</td>
+            <td>${p.situacao}</td>
+            <td>${p.dataCadastro}</td>
+        </tr>
+    `
+        // Agora adiciono cada item no array que aparece na listagem
+        template.push(cadastro)
+    ;
+    
+    // e por fim coloco esse array de texto dentro do html do tbody no html
+    document.getElementById("corpo-tabela").innerHTML = template
+    });
 }
