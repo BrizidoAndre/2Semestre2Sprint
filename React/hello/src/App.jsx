067dcd2dda1b57a092aa8165dc3dd19c6e1477be
Andre <abrizidobasilio@gmail.com>
2023-10-30 09:25:25 -0300

    feito projeto de contador no exercício em sala

--- React/hello/src/App.jsx
+++ React/hello/src/App.jsx
@@ -2,6 +2,7 @@ import './App.css';
 import CardEvento from './components/CardEvento/CardEvento';
 import Title from "./components/Title/Title";
 import Container from './components/Container/Container';
+import Contador from './components/Contador/Contador';
 
 function App() {
   return (
@@ -13,6 +14,10 @@ function App() {
       <CardEvento titulo="SQL Server" descricao="Evento referente ao como que fala quando você está iniciando introdução ao banco de dados sql server" link="Assinar"/>
       <CardEvento titulo="C#" descricao="Evento de treineiros C#" link="Conectar" />
       <CardEvento/>
+     </Container>
+
+     <Container>
+      <Contador/>
      </Container>
     </div>
   );
