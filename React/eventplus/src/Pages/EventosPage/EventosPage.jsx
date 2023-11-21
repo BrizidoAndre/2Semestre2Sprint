4aa38b099abfcef9bda003f92a0bdbb7bfece5ad
Andre <abrizidobasilio@gmail.com>
2023-11-21 10:55:35 -0300

    finalizado o método spinning, componente de carregamento

--- React/eventplus/src/Pages/EventosPage/EventosPage.jsx
+++ React/eventplus/src/Pages/EventosPage/EventosPage.jsx
@@ -1,11 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './EventosPage.css'
 
+// Importando Imagens
+import eventoImage from '../../assets/images/evento.svg'
+
+// Importando Componentes
+import Container from '../../components/Container/Container';
+import ImageIlustrator from '../../components/ImageIlustrator/ImageIlustrator';
+import MainContent from '../../components/MainContent/MainContent';
+import Title from '../../components/Title/Title';
+import { Input } from '../../components/FormComponents/FormComponents';
+
+
 const EventosPage = () => {
+    const [frmEdit, setFrmEdit] = useState(true); //Está cadastrando?
+
+
     return (
-        <div>
-            <h1>Eventos Page</h1>
-        </div>
+        <>
+            <MainContent>
+                <section className="cadastro-evento-section">
+                    <Container>
+                        <div className="cadastro-evento__box">
+
+
+                            <Title titleText={"Cadastro Eventos"}/>
+                            <ImageIlustrator imageRender={eventoImage}/>
+
+                            <form className='ftipo-evento'>
+
+                                {/* Cadastrar ou editar? */}
+                                {frmEdit ? (
+                                    <>
+                                    <Input />
+                                    </>
+                                ):(
+                                    <>
+                                    </>
+                                )}
+
+
+                            </form>
+
+
+                        </div>
+                    </Container>
+                </section>
+            </MainContent>
+        </>
     );
 };
 
